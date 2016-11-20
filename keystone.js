9675c529cbca505f5b6877ce765195a714fbbc5b
tobby2002 <tobby2002@gmail.com>
2016-11-21 02:32:08 +0900

    v0.1

--- keystone.js
+++ keystone.js
@@ -80,8 +80,8 @@ keystone.set('email locals', {
 	utils: keystone.utils,
 	host: (function() {
 		if (keystone.get('env') === 'staging') return 'http://sydjs-beta.herokuapp.com';
-		if (keystone.get('env') === 'production') return 'http://www.sydjs.com';
-		return (keystone.get('host') || 'http://localhost:') + (keystone.get('port') || '3000');
+		if (keystone.get('env') === 'production') return 'http://ai.mrtalk.co.kr';
+		return (keystone.get('host') || 'http://localhost:') + (keystone.get('port') || '80');
 	})()
 });
 
--- public/js/views/index.js
+++ public/js/views/index.js
@@ -1,3 +1,5 @@
+
+
 jQuery(function($) {
 	// MAP STYLES
 	var styles = {
