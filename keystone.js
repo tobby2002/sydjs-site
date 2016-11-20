229a14e4c3e8a25420daf5b3d27aba635d62ebee
tobby2002 <tobby2002@gmail.com>
2016-11-21 04:08:26 +0900

    v0.1

--- keystone.js
+++ keystone.js
@@ -81,7 +81,7 @@ keystone.set('email locals', {
 	host: (function() {
 		if (keystone.get('env') === 'staging') return 'http://sydjs-beta.herokuapp.com';
 		if (keystone.get('env') === 'production') return 'http://ai.mrtalk.co.kr';
-		return (keystone.get('host') || 'http://localhost:') + (keystone.get('port') || '80');
+		return (keystone.get('host') || 'http://localhost:') + (keystone.get('port') || '3000');
 	})()
 });
 
